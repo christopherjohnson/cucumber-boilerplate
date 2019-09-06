ffb48d0edcfe8a353d640890654e6a8563d5230e
Christopher Johnson <cjohnson702@gmail.com>
2019-09-06 03:22:45 -0400

    moduled changes

--- sfdx.js
+++ sfdx.js
@@ -1,82 +1,84 @@
 const childProcess = require("child_process");
 const url = require("url");
 
-//var Promise = require("bluebird"); // Bluebird is a promise library.    var fs = require('fs'),
-//var es = require("event-stream"),
-//    JSONStream = require("JSONStream");
+let baseUrl;
+let sessionPath;
+let sessionId;
 
-module.exports = (function() {
-    let baseUrl;
-    let sessionPath;
-    let sessionId;
+const getBaseUrl = function() {
+    if (baseUrl === undefined) {
+        getSession();
+    }
+    return baseUrl;
+};
 
-    let init = function() {
-        /*
-        var jwtOAuthOrg = [
-            "force:auth:jwt:grant",
-            "--clientid",
-            "3MVG9yZ.WNe6byQD2iMzzDcneLv1EUGD8f33sTa3J.qgqof9QqJJEQTTf8lY8RPn7j4qWobBFnmz7HtSGYeze",
-            "--jwtkeyfile",
-            "../server.key",
-            "--username",
-            "as.auto.test@as.com",
-            "--loglevel",
-            "TRACE",
-            "--json"
-        ];
+const getSession = function() {
+    /*
+    var jwtOAuthOrg = [
+        "force:auth:jwt:grant",
+        "--clientid",
+        "3MVG9yZ.WNe6byQD2iMzzDcneLv1EUGD8f33sTa3J.qgqof9QqJJEQTTf8lY8RPn7j4qWobBFnmz7HtSGYeze",
+        "--jwtkeyfile",
+        "../server.key",
+        "--username",
+        "as.auto.test@as.com",
+        "--loglevel",
+        "TRACE",
+        "--json"
+    ];
 
-        var jwtOAuthOrgJSON = childProcess.execFileSync("sfdx", jwtOAuthOrg, {
-            encoding: "utf8"
-        });
+    var jwtOAuthOrgJSON = childProcess.execFileSync("sfdx", jwtOAuthOrg, {
+        encoding: "utf8"
+    });
 
-        try {
-            //console.log(jwtOAuthOrgJSON)
-            instanceUrl = JSON.parse(jwtOAuthOrgJSON).result.instanceUrl;
-            /*console.log(
-                "\n\n\n\n\n\n\n\nresult " +
-                    JSON.stringify(
-                        JSON.parse(jwtOAuthOrgJSON).result.instanceUrl,
-                        null,
-                        4
-                    )
-            );
-            console.log("\n\n\n\n\n\n\n\n\ninstanceUrl : " + instanceUrl);
-        } catch (err) {}*/
+    try {
+        //console.log(jwtOAuthOrgJSON)
+        instanceUrl = JSON.parse(jwtOAuthOrgJSON).result.instanceUrl;
+        /*console.log(
+            "\n\n\n\n\n\n\n\nresult " +
+                JSON.stringify(
+                    JSON.parse(jwtOAuthOrgJSON).result.instanceUrl,
+                    null,
+                    4
+                )
+        );
+        console.log("\n\n\n\n\n\n\n\n\ninstanceUrl : " + instanceUrl);
+    } catch (err) {}*/
 
-        let orgOpen = ["force:org:open", "-u", "as.auto.test@as.com", "-r"];
-        let orgOpenResult = childProcess.execFileSync("sfdx", orgOpen, {
-            encoding: "utf8"
-        });
+    let orgOpen = ["force:org:open", "-u", "as.auto.test@as.com", "-r"];
+    let orgOpenResult = childProcess.execFileSync("sfdx", orgOpen, {
+        encoding: "utf8"
+    });
 
-        let sessionLoginUrl = orgOpenResult
-            .toString("utf8")
-            .trim()
-            .split(" ")[10];
-        console.log(sessionLoginUrl);
+    let sessionLoginUrl = orgOpenResult
+        .toString("utf8")
+        .trim()
+        .split(" ")[10];
+    //console.log(sessionLoginUrl);
 
-        const orgURL = new URL(sessionLoginUrl);
-        baseUrl = orgURL.origin;
-        sessionPath = orgURL.pathname;
-        sessionId = orgURL.searchParams.get("sid");
+    const orgURL = new URL(sessionLoginUrl);
+    baseUrl = orgURL.origin;
+    sessionPath = orgURL.pathname;
+    sessionId = orgURL.searchParams.get("sid");
 
-        console.log("this is the baseUrl ", baseUrl);
-        console.log("this is the sessionPath ", sessionPath);
-        console.log("this is the sessionId ", sessionId);
-    };
+    //console.log("this is the baseUrl ", baseUrl);
+    //console.log("this is the sessionPath ", sessionPath);
+    //console.log("this is the sessionId ", sessionId);
+};
 
-    const getSessionLoginUrl = function(brower) {
-        console.log("orgLogin ");
-        const orgURL = new URL(baseUrl + sessionPath);
-        //const newSearchParams = new URLSearchParams(orgURL.searchParams);
-        orgURL.searchParams.append("sid", sessionId);
-        console.log("orgLogin " + orgURL.href);
-        return orgURL.href;
-    };
+const getSessionLoginUrl = function() {
+    if (baseUrl === undefined) {
+        getSession();
+    }
+    //console.log("orgLogin ");
+    const orgURL = new URL(baseUrl + sessionPath);
+    //const newSearchParams = new URLSearchParams(orgURL.searchParams);
+    orgURL.searchParams.append("sid", sessionId);
+    //console.log("orgLogin " + orgURL.href);
+    return orgURL.href;
+};
 
-    init();
-
-    return {
-        baseUrl: baseUrl,
-        getSessionLoginUrl: getSessionLoginUrl
-    };
-})();
+module.exports = {
+    getSessionLoginUrl,
+    getBaseUrl
+};
